5ecb9dbeca4c588150159f82550f6e1a664692cb
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-01-30 16:46:10 +0530

    Create vi.c

---
+++ vi.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the integer \n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the number is even \n");
+}
+else
+{
+printf("the number is odd \n");
+}
+return 0;
+}
+
