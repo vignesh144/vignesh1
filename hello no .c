fc7c9d253d22cb69fa20301ed4bfd9b86d261066
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-06 16:09:52 +0530

    Create hello no .c

---
+++ hello no .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() 
+{
+    int i,j;
+    printf("enter a hello");
+    scanf("%d",&i,&j);
+    for(i=1;i<=1;i++)
+    {
+        for(j=1;j<=3;j++)
+        printf("hello");
+        return 0;
+    }
+}
