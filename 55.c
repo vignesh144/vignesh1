c68a72d78e26b5d61eef65accf3ea9ea95562a74
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-08 16:50:18 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int i,a,b=0;
+    printf("enter the number");
+    scanf("%d",&a);
+    for(i=1;i<=5;i++)
+    {
+    b=i*a;
+    printf("\n %d",b);
+    }
+}
