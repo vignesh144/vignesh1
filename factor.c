42aba94eb32bfaf9ba96a1d9be51a52770867f41
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-13 16:26:13 +0530

    Create factor.c

---
+++ factor.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,fact=1;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact*=i;
+    }
+     printf("%d",fact);
+}
