1f1bb89d9853f53ce11c555746138cd596a26301
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-13 16:39:05 +0530

    Create number5.c

---
+++ number5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,mu=1;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=5;i++)
+    {
+        mu=n*i;
+     printf("%d\t",mu);
+}}
