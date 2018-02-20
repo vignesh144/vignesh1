b4a43c17047f4e9478802a35c534f00e3f80f982
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-20 16:21:14 +0530

    Create primenum.c

---
+++ primenum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,not_prime=0;
+    printf("enter a positiveintrger");
+    scanf("%d",&n);
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        
+           not_prime=1;
+        
+    }
+    if(not_prime==0)
+      printf("%d is a primenumber",n);
+    else
+      printf("%d is not a prime number",n);
+      return 0;
+}
