f8f0b894c03ec02a5fb24c5a171ae9b26b7038c0
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-02-20 16:44:02 +0530

    Create oddnumbersbetween.c

---
+++ oddnumbersbetween.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b,i;
+    printf("enter the vales",a,b);
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2!=0)
+        
+            printf("%d",i);
+        }
+        
+    }
+    
